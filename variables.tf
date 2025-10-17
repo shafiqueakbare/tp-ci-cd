b5110d55d990223c274b7d5ecedd14caa1f186c2
Shafique Akbare <shafique.akbare@gmail.com>
2025-10-17 04:43:18 +0000

    change variables env and dev

--- variables.tf
+++ variables.tf
@@ -1,8 +1,7 @@
-variable "resource_group_name" { type = string }
-variable "location"            { type = string }
-envs/dev.tfvars :
-resource_group_name = "rg-dev-ci"
-location            = "France Central"
-envs/prod.tfvars :
-resource_group_name = "rg-prod-ci"
-location            = "West Europe"
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
